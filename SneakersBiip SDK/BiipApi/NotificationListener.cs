89dfa0ac41f5569242d1b17a35a4442283c8a685
giorgi chxikvadze <giorgi_chxikva@outlook.com>
2020-03-10 23:15:13 +0400

    Add project files.

---
+++ SneakersBiip SDK/BiipApi/NotificationListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SneakersBiip.BiipApi
+{
+    public static  class NotificationListener
+    {
+        public static string ApiToken { get; set; }
+
+        public static event BiipEventHandler NotificationReceived;
+
+        private static HubConnection _connection;
+        private static IHubProxy _hubProxy;
+
+        public static void StartListening()
+        {
+            var queryString = new Dictionary<string, string>(){ {"token", ApiToken}};
+            _connection = new HubConnection("https://stormaio.net/", queryString);
+            _hubProxy= _connection.CreateHubProxy("BiipApi");
+            _hubProxy.On<Product>("ProductUpdated", product => NotificationReceived?.Invoke(product));
+            _connection.Start();
+        }
+
+        public static void StopListening()
+        {
+            _connection?.Dispose();
+        }
+    }
+
+    public delegate void BiipEventHandler(Product product);
+}
---
+++ SneakersBiip SDK/BiipApi/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakersBiip.BiipApi
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string ShopLink { get; set; }
+
+        public string Link { get; set; }
+
+        public string Title { get; set; }
+        public string ImageLink { get; set; }
+        public double Price { get; set; }
+        public string Currency { get; set; }
+
+        public NotificationType UpdateType { get; set; }
+
+        public List<Variant> Variants { get; set; }
+
+    }
+
+    public enum NotificationType
+    {
+        Release,
+        Restock,
+    }
+
+    public class Variant
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+        public string Atc { get; set; }
+        public string Sku { get; set; }
+    }
+}
